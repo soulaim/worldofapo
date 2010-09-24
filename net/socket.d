f44637a6c4244c8db35fa0e99acf8424ef8d9cf1
Mika Urtela <urtela@dcb-10.cs.helsinki.fi>
2010-09-25 00:29:35 +0300

    Erased trash

--- net/socket.d
+++
@@ -1,2 +0,0 @@
-src/net/socket.o src/net/socket.d: src/net/socket.cpp src/net/socket.h \
- src/net/socket_handler.h
--- net/socket_handler.d
+++
@@ -1,2 +0,0 @@
-src/net/socket_handler.o src/net/socket_handler.d: \
- src/net/socket_handler.cpp src/net/socket_handler.h src/net/socket.h
