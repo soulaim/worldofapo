2ca34efee328dd226718d70c7cea1349a048a30f
DaeD <eat@spam.com>
2011-01-15 05:52:03 +0200

    Script for packaging for windows.

---
+++ package-windows.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#cd bin/;
+
+PREFIX=worldofapo
+PACKAGE=worldofapo.zip
+FILES="worldofapo/client.exe worldofapo/server.exe worldofapo/*.dll worldofapo/configs/*.conf.default worldofapo/models/* worldofapo/screenshots/ worldofapo/shaders/* worldofapo/sounds/* worldofapo/data/*/* worldofapo/serverdata/*/*"
+EXCLUDE="screenshots/*.bmp screenshots/*.png data/*.ogg old_content *.skeleton"
+
+rm -f ${PACKAGE}
+ln -s bin/ ${PREFIX}
+zip ${PACKAGE} ${FILES} -x ${EXCLUDE}
+rm -f ${PREFIX}
+
