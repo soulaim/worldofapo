27c82fcb548e20daf3cbc43142c4ebed728f92c2
DaeD <eat@spam.com>
2011-01-15 06:07:20 +0200

    Instafixed windows packager with missing file.

--- package-windows.sh
+++ package-windows.sh
@@ -4,7 +4,7 @@
 
 PREFIX=worldofapo
 PACKAGE=worldofapo.zip
-FILES="worldofapo/client.exe worldofapo/server.exe worldofapo/*.dll worldofapo/configs/*.conf.default worldofapo/models/* worldofapo/screenshots/ worldofapo/shaders/* worldofapo/sounds/* worldofapo/data/*/* worldofapo/serverdata/*/*"
+FILES="worldofapo/client.exe worldofapo/server.exe worldofapo/*.dll worldofapo/configs/*.conf.default worldofapo/models/* worldofapo/screenshots/ worldofapo/shaders/* worldofapo/sounds/* worldofapo/data/* worldofapo/data/*/* worldofapo/serverdata/*/*"
 EXCLUDE="screenshots/*.bmp screenshots/*.png data/*.ogg old_content *.skeleton"
 
 rm -f ${PACKAGE}
