5a846ee5ed001e6cbdb72b860bb184f6e51b86b4
Mika Urtela <mika.urtela@soulaimstudios.com>
2011-10-09 23:10:39 +0300

    Fixed shaders to work with new linux drivers.

--- src/world/objects/items/ballistic.cpp
+++ src/world/objects/items/ballistic.cpp
@@ -17,6 +17,15 @@ projectile.velocity = projectile_direction * FixedPoint(9, 2);
 projectile.tick();
 */
 
+Location getSpread(int spread, int bullet_num, int id) {
+    int val1 = (id * 7107 + bullet_num * 1743) % (2 * spread) - spread;
+    int val2 = (id * 5023 + bullet_num * 1217) % (2 * spread) - spread;
+    int val3 = (id * 1451 + bullet_num * 7329) % (2 * spread) - spread;
+
+    return Location(FixedPoint(val1, 10), FixedPoint(val2, 10), FixedPoint(val3, 10));
+}
+
+
 void BallisticWeaponUsage::getDetails(WorldItem* item, std::vector<std::string>& details) {
     std::stringstream ss_name;
     ss_name << "^YWeapon: ^R" << item->strVals["NAME"] << " ^w(^GBallistic^w)";
@@ -39,6 +48,9 @@ void BallisticWeaponUsage::getDetails(WorldItem* item, std::vector<std::string>&
     std::stringstream ss_dps;
     ss_dps << "^YTotal DPS: ^R" << (item->intVals["DAMAGE"] * item->intVals["BPS"] / (item->intVals["COOLDOWN"] / 25.0f));
 
+    std::stringstream ss_spread;
+    ss_spread << "^YProjectile spread: ^R" << item->intVals["SPREAD"];
+
     details.push_back(ss_name.str());
     details.push_back(ss_clip.str());
     details.push_back(ss_cooldown.str());
@@ -46,6 +58,7 @@ void BallisticWeaponUsage::getDetails(WorldItem* item, std::vector<std::string>&
     details.push_back(ss_bullets.str());
     details.push_back(ss_damage.str());
     details.push_back(ss_dps.str());
+    details.push_back(ss_spread.str());
 }
 
 void BallisticWeaponUsage::tick(WorldItem* item, Unit&) {
@@ -85,28 +98,32 @@ void BallisticWeaponUsage::onActivatePrimary(WorldItem* item, World& world, Unit
     item->intVals["CD"] = item->intVals["COOLDOWN"];
     --item->intVals["CLIP"];
 
-    int id = world.nextUnitID();
 
     Location eyePos = caster.getEyePosition();
-    world.addProjectile(eyePos, id, VisualWorld::INVISIBLE_MODEL);
-    Projectile& p = world.projectiles[id];
+    int bullets = item->intVals["BPS"];
 
-    Location direction = caster.getLookDirection();
-    caster.velocity -= direction * FixedPoint(5, 100);
+    for(int i=0; i<bullets; ++i) {
+        int id = world.nextUnitID();
+        world.addProjectile(eyePos, id, VisualWorld::INVISIBLE_MODEL);
+        Projectile& p = world.projectiles[id];
 
-    p.velocity = direction;
-    p.velocity.normalize();
-    p.velocity *= FixedPoint(9, 2);
-    p.tick();
-    p.velocity *= FixedPoint(2, 9);
+        Location direction = caster.getLookDirection();
+        caster.velocity -= direction * FixedPoint(1, 100);
 
-    p["OWNER"] = caster.id;
-    p("NAME") = (*item)("NAME");
+        p.velocity = direction + getSpread(item->intVals["SPREAD"], i, id);
+        p.velocity.normalize();
+        p.velocity *= FixedPoint(9, 2);
+        p.tick();
+        p.velocity *= FixedPoint(2, 9);
 
-    p["DAMAGE"] = 100;
-    p["BALLISTIC"] = 1;
-    p["TPF"] = 15;
-    p["DEATH_IF_HITS_UNIT"] = 1;
-    p["LIFETIME"] = 5;
-    p["MASS"] = 50;
+        p["OWNER"] = caster.id;
+        p("NAME") = (*item)("NAME");
+
+        p["DAMAGE"] = item->intVals["DAMAGE"];
+        p["BALLISTIC"] = 1;
+        p["TPF"] = 15;
+        p["DEATH_IF_HITS_UNIT"] = 1;
+        p["LIFETIME"] = 5;
+        p["MASS"] = 50;
+    }
 }
