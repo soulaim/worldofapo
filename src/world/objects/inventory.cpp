147308c5b224c54857f0f69df6476b01838b8d7f
Mika Urtela <mika.urtela@soulaimstudios.com>
2011-11-17 16:11:54 +0200

    maybe fix

--- src/world/objects/inventory.cpp
+++ src/world/objects/inventory.cpp
@@ -250,15 +250,16 @@ void Inventory::dropItemSlot(World& world, Unit& unit, int i) {
     if(this->wieldedItems[i] == 0)
         return;
 
-    this->wieldedItems[i]->position = unit.getEyePosition();
-
     RandomMachine random;
     random.setSeed(unit.id);
     FixedPoint x = FixedPoint( (random.getInt() % 10) - 5, 10 );
     FixedPoint z = FixedPoint( (random.getInt() % 10) - 5, 10 );
     FixedPoint y = FixedPoint( (random.getInt() % 5)  + 1, 10 );
-    this->wieldedItems[i]->velocity = Location(x, y, z);
-    world.addItem(*(this->wieldedItems[i]), VisualWorld::ModelType(this->wieldedItems[i]->intVals["MODEL_TYPE"]), world.nextUnitID());
+
+    int id = world.nextUnitID();
+    world.addItem(*(this->wieldedItems[i]), VisualWorld::ModelType(this->wieldedItems[i]->intVals["MODEL_TYPE"]), id);
+    world.items[id].velocity = Location(x, y, z);
+    world.items[id].position = unit.getEyePosition();
 
     delete this->wieldedItems[i];
     this->wieldedItems[i] = 0;
