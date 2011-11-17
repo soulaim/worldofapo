e327e6f3c7d5a7e13e96ff6c4e96c558ebfebb3c
Mika Urtela <mika.urtela@soulaimstudios.com>
2011-11-17 14:58:51 +0200

    for pessi

--- src/world/logic/playerdeath.cpp
+++ src/world/logic/playerdeath.cpp
@@ -2,11 +2,17 @@
 #include "world/logic/playerdeath.h"
 #include "world/world.h"
 #include "world/objects/unit.h"
+#include <sstream>
 
 void PlayerDeathHandler::handle(World& world, Unit& unit) {
-    unit["HEALTH"] = unit.getMaxHP();
 
+    std::stringstream ss; ss << "^G" << unit.name << " ^Rhas died!";
+    world.add_message(ss.str());
+
+    /*
+    unit["HEALTH"] = unit.getMaxHP();
     Location pos = world.lvl.getStartLocation();
     unit.setPosition(pos);
     unit.zeroMovement();
+    */
 }
